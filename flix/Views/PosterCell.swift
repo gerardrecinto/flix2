0d11db75dc1df02195e248677c74f2137aadc103
gerardrecinto <grecinto@ucsd.edu>
2017-11-24 02:32:44 -0700

    finished for now

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Gerard Recinto on 11/24/17.
+//  Copyright © 2017 Gerard Recinto. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+  @IBOutlet weak var posterImageView: UIImageView!
+  
+}
