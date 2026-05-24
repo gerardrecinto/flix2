f5d1027ad99c8057ab030d8fa20b33a57b690f07
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:55:44 -0700

    fix: replace force-casts with safe casts, add @MainActor, guard weak self in closure

--- flix/View Controllers/SuperheroViewController.swift
+++ flix/View Controllers/SuperheroViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@MainActor
 class SuperheroViewController: UIViewController, UICollectionViewDataSource {
 
   @IBOutlet weak var collectionView: UICollectionView!
@@ -18,45 +19,38 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
   override func viewDidLoad() {
     super.viewDidLoad()
     collectionView.dataSource = self
-    let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-    layout.minimumInteritemSpacing = 5
-    layout.minimumLineSpacing = layout.minimumInteritemSpacing
-    let cellsPerLine: CGFloat = 3
-    let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
-    let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
-    layout.itemSize = CGSize(width: width, height: width * 3/2)
+    if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+      layout.minimumInteritemSpacing = 5
+      layout.minimumLineSpacing = layout.minimumInteritemSpacing
+      let cellsPerLine: CGFloat = 3
+      let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+      let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+      layout.itemSize = CGSize(width: width, height: width * 3/2)
+    }
     fetchMovies()
-    // Do any additional setup after loading the view.
   }
 
 
   func fetchMovies(){
-    let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
+    guard let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1") else { return }
     let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     let task = session.dataTask(with: request) { [weak self] (data, response, error) in
-      // This will run when the network request returns
+      guard let self = self else { return }
       if let error = error {
         print(error.localizedDescription)
       } else if let data = data {
-        //self.activityIndicator.startAnimating()
         guard let dataDictionary = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { return }
-
-        // TODO: Get the array of movies
-        let movies = dataDictionary["results"] as! [[String: Any]]
+        guard let movies = dataDictionary["results"] as? [[String: Any]] else { return }
         self.movies = movies
         self.collectionView.reloadData()
-        //  self.refreshControl.endRefreshing()
-        // TODO: Store the movies in a property to use elsewhere
         for movie in movies {
-          let title = movie["title"] as! String
+          let title = movie["title"] as? String ?? ""
           print(title)
         }
-        // TODO: Reload your table view data
-        //   self.activityIndicator.stopAnimating()
       }
     }
-    task.resume()        // Do any additional setup after loading the view.
+    task.resume()
   }
 
 
@@ -66,9 +60,8 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
     let movie = movies[indexPath.item]
-    if let posterPathString = movie[MovieKeys.posterPath] as? String{
-      let baseURLString = "https://image.tmdb.org/t/p/w500"
-      let posterURL = URL(string: baseURLString + posterPathString)!
+    if let posterPathString = movie[MovieKeys.posterPath] as? String,
+       let posterURL = URL(string: "https://image.tmdb.org/t/p/w500" + posterPathString) {
       cell.posterImageView.loadImage(from: posterURL)
     }
     return cell
@@ -80,15 +73,12 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
 
   // MARK: - Navigation
 
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    let cell = sender as! UICollectionViewCell
-    if let indexPath = collectionView.indexPath(for: cell){
-      let movie = movies[indexPath.item]
-      let detailViewController = segue.destination as! DetailViewController
+    guard let cell = sender as? UICollectionViewCell,
+          let indexPath = collectionView.indexPath(for: cell) else { return }
+    let movie = movies[indexPath.item]
+    if let detailViewController = segue.destination as? DetailViewController {
       detailViewController.movie = movie
-      // Get the new view controller using segue.destinationViewController.
-      // Pass the selected object to the new view controller.
     }
   }
 
